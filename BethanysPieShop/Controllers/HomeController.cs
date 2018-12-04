0324b93f6dda3360d9c03881c8d4714ffb6fa995
Tania Chattopadhyay <tania_chattopadhyay@iras.gov.sg>
2018-12-04 23:08:00 +0800

    Add views and controllers

--- BethanysPieShop/Controllers/HomeController.cs
+++ BethanysPieShop/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using BethanysPieShop.Models;
+using BethanysPieShop.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -21,7 +22,25 @@ namespace BethanysPieShop.Controllers
         // GET: /<controller>/
         public IActionResult Index()
         {
-            return View();
+            var pies = _pieRepository.GetAllPies().OrderBy(p => p.Name);
+
+            var homeViewModel = new HomeViewModel()
+            {
+                Title = "Welcome to Bethany's Pie Shop",
+                Pies = pies.ToList()
+            };
+
+            return View(homeViewModel);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var pie = _pieRepository.GetPieById(id);
+
+            if (pie == null)
+                return NotFound();
+
+            return View(pie);
         }
     }
 }
---
+++ BethanysPieShop/Views/Feedback/FeedbackComplete.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BethanysPieShop.Views.Feedback
+{
+    public class FeedbackCompleteModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
